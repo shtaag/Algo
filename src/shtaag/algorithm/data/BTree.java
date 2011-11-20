be67025a282602aff3b21536c606237f6ffe51d6
Shigekazu Takei <takei.shg@gmail.com>
2011-11-21 02:24:30 +0900

    submit B-Tree

--- src/shtaag/algorithm/data/BTree.java
+++ src/shtaag/algorithm/data/BTree.java
@@ -3,7 +3,9 @@
  */
 package shtaag.algorithm.data;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Stack;
 
@@ -12,6 +14,7 @@ import java.util.Stack;
  * @Date 2011/11/12
  * 
  */
+@SuppressWarnings("unchecked")
 public class BTree<K extends Comparable<K>, V> implements Iterable<V> {
 	
 	final int minDegree;
@@ -20,18 +23,18 @@ public class BTree<K extends Comparable<K>, V> implements Iterable<V> {
 	
 	private static class Node<K, V> {
 		
-		boolean isLeaf;
+		final boolean isLeaf;
 		boolean isRoot;
 		//TODO why this cant be Element<K,V>[]??
 		Object[] elementArray;
 		Object[] childArray;
 		int elementSize;
 		int childSize;
-		Node(int minDegree) {
+		Node(boolean isLeaf, int minDegree) { // staticなのでminDegreeを渡さなければならない
+			this.isLeaf = isLeaf;
 			elementArray = new Object[minDegree*2-1];
-			childArray = new Object[minDegree*2-1];
+			childArray = new Object[minDegree*2];
 		}
-		
 	}
 	
 	private static class Element<K, V> {
@@ -45,8 +48,7 @@ public class BTree<K extends Comparable<K>, V> implements Iterable<V> {
 	
 	public BTree(int minDegree) {
 		this.minDegree = minDegree;
-		nodeInfo = new Node<K, V>(minDegree);
-		nodeInfo.isLeaf = true;
+		nodeInfo = new Node<K, V>(true, minDegree);
 		root = nodeInfo;
 		nodeInfo.isRoot = true;
 		nodeInfo.elementSize = 0;
@@ -54,19 +56,57 @@ public class BTree<K extends Comparable<K>, V> implements Iterable<V> {
 	}
 	
 	public V search(K key) {
-		return btreeSearch(root, key);
+		return searchRecursive(root, key);
 	}
-	private V btreeSearch(Node<K, V> node, K key) {
+	private V searchRecursive(Node<K, V> node, K key) {
 		int i = 0;
-		while (i <= node.elementSize && key.compareTo(((Element<K, V>)node.elementArray[i]).key) > 0) {
+		while (i <= node.elementSize - 1 && key.compareTo(((Element<K, V>)node.elementArray[i]).key) > 0) {
 			i++;
 		}
-		if (i <= node.elementSize && key.equals(((Element<K, V>)node.elementArray[i]).key) ) {
+		if (i <= node.elementSize - 1 && key.equals(((Element<K, V>)node.elementArray[i]).key) ) {
 			return ((Element<K, V>)node.elementArray[i]).value;
 		} else if (node.isLeaf) {
 			throw new NoSuchElementException();
 		} else {
-			return btreeSearch((Node<K, V>) node.childArray[i], key);
+			return searchRecursive((Node<K, V>) node.childArray[i], key); // child exists between key and index
+		}
+	}
+	private int searchCurrentNode(Node<K, V> node, K key) {
+		int i = 0;
+		while (i <= node.elementSize - 1 && key.compareTo(((Element<K, V>)node.elementArray[i]).key) > 0) {
+			i++;
+		}
+		if (i <= node.elementSize - 1 && key.equals(((Element<K, V>)node.elementArray[i]).key) ) {
+			return i;
+		} else {
+			throw new NoSuchElementException();
+		} 
+	}
+	private static class ResultTaple<K, V> {
+		final Node<K, V> parent;
+		final int keyIndex;
+		final int childIndex;
+		ResultTaple(Node<K,V> parent, int keyIndex, int childIndex) {
+			this.parent = parent;
+			this.keyIndex = keyIndex;
+			this.childIndex = childIndex;
+		}
+	}
+	private ResultTaple<K, V> searchChildToDelete(Node<K, V> parent, int childIndex, K key) {
+		int i = 0;
+		Node<K, V> node = (Node<K, V>)parent.childArray[childIndex];
+		if (node == null) {
+			return null;
+		}
+		while (i <= node.elementSize - 1 && key.compareTo(((Element<K, V>)node.elementArray[i]).key) > 0) {
+			i++;
+		}
+		if (i <= node.elementSize - 1 && key.equals(((Element<K, V>)node.elementArray[i]).key) ) {
+			return new ResultTaple<K, V>(parent, i, childIndex);
+		} else if (node.isLeaf) {
+			return null;
+		} else {
+			return searchChildToDelete(node, i, key); // child exists between key and index
 		}
 	}
 
@@ -126,9 +166,8 @@ public class BTree<K extends Comparable<K>, V> implements Iterable<V> {
 	public void insert(final BTree<K, V> tree, K key, V value) {
 		Node<K, V> exRoot = tree.root;
 		if (tree.root.elementSize == minDegree * 2 -1) {
-			Node<K, V> newNode = new Node<K, V>(minDegree);
+			Node<K, V> newNode = new Node<K, V>(false, minDegree);
 			tree.root = newNode;
-			newNode.isLeaf = false;
 			newNode.elementSize = 0;
 			newNode.childArray[0] = exRoot;
 			newNode.childSize = 1;
@@ -143,15 +182,21 @@ public class BTree<K extends Comparable<K>, V> implements Iterable<V> {
 		int index = node.elementSize - 1;
 		if (node.isLeaf) {
 			if (index != -1) {
-				while (index >= 0 && key.compareTo(((Element<K,V>)node.elementArray[index]).key) < 0) {
+				while (index >= 0 && key.compareTo(((Element<K,V>)node.elementArray[index]).key) <= 0) {
+					if (key.equals(((Element<K,V>)node.elementArray[index]).key)) {
+						throw new IllegalArgumentException("There already exists same key.");
+					}
 					node.elementArray[index + 1] = node.elementArray[index];
 					index--;
 				}
 			}
-			node.elementArray[index+1] = new Element<K, V>(key, value);
+			node.elementArray[index + 1] = new Element<K, V>(key, value);
 			node.elementSize++;
 		} else {
-			while (index >= 0 && key.compareTo(((Element<K,V>) node.elementArray[index]).key) < 0) {
+			while (index >= 0 && key.compareTo(((Element<K,V>) node.elementArray[index]).key) <= 0) {
+				if (key.equals(((Element<K,V>)node.elementArray[index]).key)) {
+					throw new IllegalArgumentException("There already exists same key.");
+				}
 				index--;
 			}
 			index++;
@@ -166,10 +211,8 @@ public class BTree<K extends Comparable<K>, V> implements Iterable<V> {
 	}
 	
 	private void btreeSplitChild(Node<K, V> node, final int index) {
-		Node<K, V> newNode = new Node<K, V>(minDegree);
 		Node<K, V> exNode = (Node<K, V>) node.childArray[index];
-		
-		newNode.isLeaf = exNode.isLeaf;
+		Node<K, V> newNode = new Node<K, V>(exNode.isLeaf, minDegree);
 		
 		for (int j = 0; j < minDegree - 1; j++) {
 			newNode.elementArray[j] = exNode.elementArray[j + minDegree];
@@ -188,20 +231,144 @@ public class BTree<K extends Comparable<K>, V> implements Iterable<V> {
 			newNode.childSize = minDegree;
 		}
 
-		for (int j = node.childSize-1; j >= index; j--) {
+		for (int j = node.childSize - 1; j >= index; j--) {
 			node.childArray[j + 1] = node.childArray[j];
 		}
 		node.childArray[index + 1] = newNode;
 		node.childSize++;
-		if (node.elementSize != 0) {
-			for (int j = node.elementSize; j >= index; j--) {
-				node.elementArray[j - 1] = node.elementArray[j - 2];
+		if (index < node.elementSize) { // certify the index is at middle of elementArray
+			for (int j = node.elementSize - 1; j >= index; j--) {
+				// index is the position in childArray. 
+				node.elementArray[j + 1] = node.elementArray[j];
 			}
 		}
-		node.elementArray[index] = exNode.elementArray[minDegree-1];
+		node.elementArray[index] = exNode.elementArray[minDegree-1]; // push up to parent node
 		exNode.elementArray[minDegree - 1] = null;
 		node.elementSize++;
 	}
+	
+	public void show() {
+		System.out.println("+++ Here's the current condition of your B-Tree!! +++");
+		List<Node<K,V>> list = new ArrayList<BTree.Node<K,V>>();
+		list.add(root);
+		showElements(list, 0);
+	}
 
-
+	private void showElements(List<Node<K, V>> nodeList, int height) {
+		StringBuilder sb = new StringBuilder();
+		List<Node<K,V>> childList = new ArrayList<Node<K,V>>();
+		sb.append("Height: " + height + " >>> ");
+		sb.append("Elems: ");
+		for (Node<K, V> node: nodeList) {
+			for (int i = 0; i < node.elementSize; i++) {
+				sb.append(((Element<K, V>)node.elementArray[i]).key + " ");
+			}
+			sb.append("|| ");
+			if (! node.isLeaf) {
+				for (int i = 0; i < node.childSize; i++) {
+					childList.add((Node<K,V>)node.childArray[i]);
+				}
+			}
+		}
+		System.out.println(sb.toString());
+		if (childList.isEmpty()) {
+			// if it is leaf, stop recurtion.
+			return;
+		}
+		height++;
+		showElements(childList, height);
+	}
+	
+	public void delete(K key) throws NoSuchElementException {
+		// in case of no target, NoSuchElementException is thrown.
+		try {
+			// search root
+			int index = searchCurrentNode(root, key);
+			//delete from root
+			if (root.isLeaf) {
+				deleteAt(root, index); //parentがないので、deleteHelpのDeleteInLeafは使えない。
+			} else {
+				deleteAtInternalNode(root, index);
+			}
+		} catch (NoSuchElementException e) { // 例外をロジックに使うべきではないが、、index == -1と比べるとまだ分り易くないか？
+			// search into child
+			ResultTaple<K, V> target = searchChildToDelete(root, 0, key);
+			deleteHelp(target, key);
+		}
+	}
+	
+	private void deleteHelp(ResultTaple<K, V> target, K key) {
+		Node<K, V> targetNode = (Node<K,V>)target.parent.childArray[target.childIndex];
+		Node<K, V> parent = target.parent;
+		int keyIndex = target.keyIndex;
+		int childIndexAtParent = target.childIndex;
+		if (! targetNode.isLeaf) {
+			deleteAtInternalNode(targetNode, keyIndex);
+		} else {
+			deleteInLeaf(targetNode, parent, keyIndex, childIndexAtParent);
+		}
+	}
+	
+	private void deleteAt(Node<K, V> node, int index) {
+		shrinkArray(node.elementArray, node.elementSize, index);
+		node.elementSize--;
+	}
+	private <E> void shrinkArray(Object[] array, int size, int index) {
+		for (int i = index; i <= size - 2; i++) {
+			array[i] = array[i + 1];
+		}
+		array[size - 1] = null;
+	}
+	
+	private void deleteAtInternalNode(
+			Node<K,V> targetNode, int keyIndex) {
+		Node<K, V> preChild = (Node<K, V>)targetNode.childArray[keyIndex];
+		Node<K, V> posChild = (Node<K, V>)targetNode.childArray[keyIndex + 1];
+		if (preChild.elementSize >= minDegree) { // Algorithm Introduction 2a
+			Element<K, V> predecessor = (Element<K, V>)preChild.elementArray[preChild.elementSize - 1];
+			targetNode.elementArray[keyIndex] = predecessor;
+			preChild.elementArray[preChild.elementSize - 1] = null;
+			preChild.elementSize--;
+		} else {
+			if (posChild.elementSize >= minDegree) { // Algorithm Introduction 2b
+				Element<K, V> successor = (Element<K, V>)posChild.elementArray[0];
+				targetNode.elementArray[keyIndex] = successor; // substitution
+				// shrink posChild elementarray
+				deleteAt(posChild, 0);
+			} else { // Algo 2c
+				// merge pre and pos
+				for (int i = 0; i < posChild.elementSize; i++) {
+					preChild.elementArray[i + preChild.elementSize] = posChild.elementArray[i];
+					posChild.elementArray[i] = null;
+				}
+				preChild.elementSize += posChild.elementSize;
+				
+				// delete key and pointer to poschild
+				shrinkArray(targetNode.elementArray, targetNode.elementSize, keyIndex);
+				shrinkArray(targetNode.childArray, targetNode.childSize, keyIndex + 1);
+				
+				targetNode.elementSize--;
+				targetNode.childSize--;
+			}
+		}
+	}
+	
+	private void deleteInLeaf(
+			Node<K, V> targetNode, Node<K, V> parent, int keyIndex, int childIndexAtParent) {
+		if (targetNode.elementSize <= minDegree - 1) {
+			// swap the target with successor
+			Element<K, V> successor = (Element<K, V>)parent.elementArray[childIndexAtParent];
+			targetNode.elementArray[keyIndex] = successor;
+			// swap the successor with successor's soccessor in next sibling
+			Node<K, V> nextSibling = (Node<K, V>)parent.childArray[childIndexAtParent + 1];
+			Element<K, V> nextSuccessor = (Element<K, V>)nextSibling.elementArray[0];
+			parent.elementArray[childIndexAtParent] = nextSuccessor;
+			// shrink next sibling
+			deleteAt(nextSibling, 0);
+		} else {
+			// delete element at keyIndex of existingNode
+			deleteAt(targetNode, keyIndex);
+		}
+	}
+	
 }
--- src/shtaag/algorithm/data/BTreeTest.java
+++ src/shtaag/algorithm/data/BTreeTest.java
@@ -7,10 +7,12 @@ import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.is;
 
 import java.util.Iterator;
+import java.util.TreeSet;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.omg.PortableInterceptor.SUCCESSFUL;
 
 /**
  * @author takei_s
@@ -37,7 +39,7 @@ public class BTreeTest {
 	 */
 	@Test
 	public void testSearch() {
-		BTree<Integer, Integer> tree = new BTree<Integer, Integer>(4);
+		BTree<Integer, Integer> tree = new BTree<Integer, Integer>(3);
 		tree.insert(tree, 2, 20);
 		tree.insert(tree, 5, 50);
 		tree.insert(tree, 3, 30);
@@ -48,6 +50,31 @@ public class BTreeTest {
 		tree.insert(tree, 4, 40);
 		tree.insert(tree, 9, 90);
 		tree.insert(tree, 10, 100);
+		tree.insert(tree, 11, 110);
+		tree.insert(tree, 12, 120);
+		tree.insert(tree, 13, 130);
+		tree.insert(tree, 14, 140);
+		tree.insert(tree, 15, 150);
+		tree.insert(tree, 16, 160);
+		tree.insert(tree, 17, 170);
+		tree.insert(tree, 18, 180);
+		tree.insert(tree, 19, 190);
+		tree.insert(tree, 20, 200);
+		tree.insert(tree, 21, 210);
+		tree.insert(tree, 22, 220);
+		tree.insert(tree, 23, 230);
+		tree.insert(tree, 24, 240);
+		tree.insert(tree, 25, 250);
+		tree.insert(tree, 26, 260);
+		tree.insert(tree, 27, 270);
+		tree.insert(tree, 28, 280);
+		tree.insert(tree, 29, 290);
+		tree.insert(tree, 30, 300);
+		tree.insert(tree, 31, 310);
+		tree.insert(tree, 32, 320);
+		tree.insert(tree, 33, 330);
+		
+		tree.show();
 		
 		assertThat(tree.search(4), is(40));
 	}
@@ -69,33 +96,109 @@ public class BTreeTest {
 		tree.insert(tree, 9, 90);
 		tree.insert(tree, 10, 100);
 		
-		for (Iterator<Integer> itr = tree.iterator(); itr.hasNext();) {
-			System.out.println(itr.next());
-		}
+		tree.show();
 		
 		assertThat(tree.search(3), is(30));
 	}
 
+	@Test
+	public void testInsertSameKey() {
+		BTree<Integer, Integer> tree = new BTree<Integer, Integer>(4);
+		try {
+			tree.insert(tree, 2, 20);
+			tree.insert(tree, 2, 20);
+		
+		} catch (IllegalArgumentException e) { 
+			assertTrue(true);
+			return;
+		}
+		fail();
+	}
 	/**
 	 * Test method for {@link shtaag.algorithm.data.BTree#iterator()}.
 	 */
 	@Test
 	public void testIterator() {
-		BTree<Integer, Integer> tree = new BTree<Integer, Integer>(4);
+		BTree<Integer, Integer> tree = new BTree<Integer, Integer>(3);
+		tree.insert(tree, 1, 10);
 		tree.insert(tree, 2, 20);
-		tree.insert(tree, 2, 20);
-		tree.insert(tree, 2, 20);
-		tree.insert(tree, 2, 20);
-		tree.insert(tree, 2, 20);
-		tree.insert(tree, 2, 20);
+		tree.insert(tree, 3, 30);
+		tree.insert(tree, 4, 40);
+		tree.insert(tree, 5, 50);
+		tree.insert(tree, 6, 60);
+		tree.insert(tree, 7, 70);
+		tree.insert(tree, 8, 80);
+		tree.insert(tree, 9, 90);
+		tree.insert(tree, 10, 100);
+		tree.insert(tree, 11, 110);
+		tree.insert(tree, 12, 120);
+		tree.insert(tree, 13, 130);
+		tree.insert(tree, 14, 140);
+		tree.insert(tree, 15, 150);
+		tree.insert(tree, 16, 160);
+		tree.insert(tree, 17, 170);
+		tree.insert(tree, 18, 180);
+		tree.insert(tree, 19, 190);
+		tree.insert(tree, 20, 200);
+		tree.insert(tree, 21, 210);
+		tree.insert(tree, 22, 220);
+		tree.insert(tree, 23, 230);
+		tree.insert(tree, 24, 240);
+		tree.insert(tree, 25, 250);
+		tree.insert(tree, 26, 260);
+		tree.insert(tree, 27, 270);
+		tree.insert(tree, 28, 280);
+		tree.insert(tree, 29, 290);
+		tree.insert(tree, 30, 300);
+		tree.insert(tree, 31, 310);
+		tree.insert(tree, 32, 320);
+		tree.insert(tree, 33, 330);
 		
+		tree.show();
 		
-		
-		for (Iterator<Integer> itr = tree.iterator(); itr.hasNext();) {
-			System.out.println(itr.next());
+		TreeSet<Integer> set = new TreeSet<Integer>();
+		for (Integer value: tree) {
+			set.add(value);
 		}
-		
-		assertThat(tree.search(2), is(20));
+		for (int i = 1; i <= 33; i++) {
+			assertThat(set.contains(Integer.valueOf(i*10)), is(true));
+		}
 	}
+	
+	@Test
+	public void testDeleteAtRootWithNoChild() {
+		BTree<Integer, Integer> tree = new BTree<Integer, Integer>(3);
+		tree.insert(tree, 5, 50);
+		tree.insert(tree, 3, 30);
+		
+		tree.show();
+		tree.delete(3);
+		
+		tree.show();
+		
+		assertThat(tree.search(5), is(50));
+		
+	}
+
+	@Test
+	public void testDeleteAtRootWithChild() {
+		BTree<Integer, Integer> tree = new BTree<Integer, Integer>(3);
+		tree.insert(tree, 5, 50);
+		tree.insert(tree, 3, 30);
+		tree.insert(tree, 2, 20);
+		tree.insert(tree, 1, 10);
+		tree.insert(tree, 6, 60);
+		tree.insert(tree, 7, 70);
+		tree.insert(tree, 8, 80);
+		
+		tree.show();
+		tree.delete(3);
+		
+		tree.show();
+		
+		assertThat(tree.search(5), is(50));
+		
+	}
+	
 
 }
