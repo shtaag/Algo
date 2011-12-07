ef2f82a4e95d5de2024f240d14ec3c74462ea667
shtaag <takei.shg@gmail.com>
2011-12-07 19:06:09 +0900

    final primitiveを追加

--- src/shtaag/misc/MockFinalField.java
+++ src/shtaag/misc/MockFinalField.java
@@ -33,18 +33,27 @@ public class MockFinalField {
 		
 		System.out.println(">> Fieldに対してsetAccessible(true)をセットする場合。");
 		
-		ToMock mock = new ToMock("fin-immutable", "pri-immutable");
+		ToMock mock = new ToMock("fin-immutable", 10, "pri-immutable");
+		
+		System.out.println(mock.getFinalField()); // >> fin-immutableが表示される。
+		System.out.println(mock.getFinalPrimitive()); // >> 10
+		System.out.println(mock.getPrivateField()); // >> pri-immutableが表示される。
 		
 		Class<? extends ToMock> clazz = mock.getClass();
 		Field fin = clazz.getDeclaredField("finalField");
 		fin.setAccessible(true);
 		fin.set(mock, "fin-mutable");
 		
+		Field finPrim = clazz.getDeclaredField("finalPrimitive");
+		finPrim.setAccessible(true);
+		finPrim.set(mock, 1000);
+
 		Field pri = clazz.getDeclaredField("privateField");
 		pri.setAccessible(true);
 		pri.set(mock, "pri-mutable");
 		
 		System.out.println(mock.getFinalField()); // >> fin-mutableが表示される。
+		System.out.println(mock.getFinalPrimitive()); // >> 1000
 		System.out.println(mock.getPrivateField()); // >> pri-mutableが表示される。
 		
 	}
@@ -53,7 +62,7 @@ public class MockFinalField {
 		
 		System.out.println(">> Fieldに対してsetAccessible(true)をセットしない場合。");
 
-		ToMock mock = new ToMock("fin-immutable", "pri-immutable");
+		ToMock mock = new ToMock("fin-immutable", 10, "pri-immutable");
 		
 		Class<? extends ToMock> clazz = mock.getClass();
 		Field fin = clazz.getDeclaredField("finalField");
@@ -63,6 +72,13 @@ public class MockFinalField {
 			System.out.println("setAccessible(true)が必要。");
 		}
 		
+		Field finPrim = clazz.getDeclaredField("finalPrimitive");
+		try {
+			finPrim.set(mock, 1000);
+		} catch (IllegalAccessException e) {
+			System.out.println("setAccessible(true)が必要。");
+		}
+
 		Field pri = clazz.getDeclaredField("privateField");
 		try {
 			pri.set(mock, "pri-mutable");
@@ -71,6 +87,7 @@ public class MockFinalField {
 		}
 		
 		System.out.println(mock.getFinalField()); // >> fin-immutableが表示される。
+		System.out.println(mock.getFinalPrimitive()); // >> 10が表示される。
 		System.out.println(mock.getPrivateField()); // >> pri-immutableが表示される。
 		
 	}
--- src/shtaag/misc/ToMock.java
+++ src/shtaag/misc/ToMock.java
@@ -11,17 +11,22 @@ package shtaag.misc;
 public class ToMock {
 	
 	private final String finalField;
+	private final int finalPrimitive;
 
 	private String privateField;
 	public Integer test = Integer.valueOf(1);
 	
-	public ToMock(String finalField, String privateField) {
+	public ToMock(String finalField, int finalPrimitive, String privateField) {
 		this.finalField = finalField;
+		this.finalPrimitive = finalPrimitive;
 		this.privateField = privateField;
 	}
 	
 	public String getFinalField() {
 		return finalField;
+	}
+	public int getFinalPrimitive() {
+		return finalPrimitive;
 	}
 	
 	public String getPrivateField() {
