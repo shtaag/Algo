fd0d362c0bcb0c7d02c6ad553d7594ba4491d913
shtaag <takei.shg@gmail.com>
2011-12-12 12:00:24 +0900

    add testdata

--- src/shtaag/algorithm/data/graph/DijkstraTest.java
+++ src/shtaag/algorithm/data/graph/DijkstraTest.java
@@ -39,15 +39,63 @@ public class DijkstraTest {
 		graph.addNode("B");
 		graph.addNode("C");
 		graph.addNode("D");
+		graph.addNode("E");
+		graph.addNode("F");
+		graph.addNode("G");
+		graph.addNode("H");
 		
 		graph.setEdge("A", "B", 1);
 		graph.setEdge("A", "C", 6);
 		graph.setEdge("A", "D", 8);
+		graph.setEdge("A", "E", 10);
+		graph.setEdge("A", "F", 20);
+		graph.setEdge("A", "G", 40);
+		graph.setEdge("A", "H", 100);
 		
+		graph.setEdge("B", "A", 2);
 		graph.setEdge("B", "C", 2);
 		graph.setEdge("B", "D", 5);
+		graph.setEdge("B", "E", 5);
+		graph.setEdge("B", "F", 5);
+		graph.setEdge("B", "G", 5);
+		graph.setEdge("B", "H", 5);
 		
+		graph.setEdge("C", "A", 1);
+		graph.setEdge("C", "B", 1);
 		graph.setEdge("C", "D", 1);
+		graph.setEdge("C", "E", 1);
+		graph.setEdge("C", "F", 1);
+		graph.setEdge("C", "G", 1);
+		graph.setEdge("C", "H", 1);
+
+		graph.setEdge("D", "A", 1);
+		graph.setEdge("D", "B", 1);
+		graph.setEdge("D", "C", 1);
+		graph.setEdge("D", "E", 1);
+		graph.setEdge("D", "F", 1);
+		graph.setEdge("D", "G", 1);
+		graph.setEdge("D", "H", 1);
+		
+		graph.setEdge("E", "A", 1);
+		graph.setEdge("E", "D", 1);
+		graph.setEdge("E", "F", 1);
+		graph.setEdge("E", "H", 1);
+
+		graph.setEdge("F", "A", 1);
+		graph.setEdge("F", "B", 1);
+		graph.setEdge("F", "C", 1);
+		graph.setEdge("F", "D", 1);
+		
+		graph.setEdge("G", "A", 1);
+		graph.setEdge("G", "D", 1);
+		graph.setEdge("G", "E", 1);
+		graph.setEdge("G", "F", 1);
+
+		graph.setEdge("H", "A", 1);
+		graph.setEdge("H", "B", 1);
+		graph.setEdge("H", "D", 1);
+		graph.setEdge("H", "F", 1);
+
 		
 		new Dijkstra<String>().search(graph, "A");
 		System.out.println(graph.toString());
